7dae255eb52a222d3a76b5cebbe305415630c0e2
coryo12345 <corydio12@gmail.com>
2024-05-14 15:21:37 -0400

    todo for stores page

--- pages/stores.vue
+++ pages/stores.vue
@@ -1,3 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// install vue-draggable-plus
+// https://github.com/Alfred-Skyblue/vue-draggable-plus
+</script>
 
 <template>manage preset orders here</template>
