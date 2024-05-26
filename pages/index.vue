a12289d512e110f8f749e69a7ba32a1425567921
coryo12345 <corydio12@gmail.com>
2024-05-26 15:53:10 -0400

    sorting works

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,43 @@
 import GroceryListItem from "~/components/GroceryListItem.vue";
 
 const { data, pending, error, refresh } = await useFetch("/api/groceries");
+const { data: stores, error: storesError } = await useFetch("/api/stores");
+
+const selectedStore = ref({ id: -1, name: "None", categories: [] as number[] });
+
+const allStores = computed(() => [
+  ...(stores.value ?? []),
+  { id: -1, name: "None" },
+]);
+
+const sortedGroceries = computed(() => {
+  const items = [...(data.value ?? [])];
+  if (selectedStore.value.id === -1) {
+    return items;
+  }
+
+  items.sort((a, b) => {
+    let aIdx = Number.MAX_SAFE_INTEGER;
+    a.all_groceries.categories?.forEach((c) => {
+      const i = selectedStore.value.categories.findIndex((sci) => sci == c);
+      if (i !== -1 && i < aIdx) aIdx = i;
+    });
+    if (aIdx === Number.MAX_SAFE_INTEGER) {
+      aIdx = 0;
+    }
+
+    let bIdx = Number.MAX_SAFE_INTEGER;
+    b.all_groceries.categories?.forEach((c) => {
+      const i = selectedStore.value.categories.findIndex((sci) => sci == c);
+      if (i !== -1 && i < bIdx) bIdx = i;
+    });
+    if (bIdx === Number.MAX_SAFE_INTEGER) {
+      bIdx = 0;
+    }
+    return aIdx - bIdx;
+  });
+  return items;
+});
 </script>
 
 <template>
@@ -19,15 +56,21 @@ const { data, pending, error, refresh } = await useFetch("/api/groceries");
       </span>
       <p>TODO: implement this list</p>
       <v-select
-        :model-value="'None'"
-        :items="['None', 'Wegmans']"
+        v-model="selectedStore"
+        :items="allStores"
+        item-title="name"
+        return-object
         variant="outlined"
         label="Choose Store (for sorting)"
         class="mt-6"
         hide-details
       />
-      <v-list v-if="data?.length" class="my-2">
-        <v-list-item v-for="(item, idx) in data" :key="idx" class="pl-0">
+      <v-list v-if="sortedGroceries.length" class="my-2">
+        <v-list-item
+          v-for="(item, idx) in sortedGroceries"
+          :key="item.grocery_list.id"
+          class="pl-0"
+        >
           <GroceryListItem :item="item" @item-deleted="refresh" />
         </v-list-item>
       </v-list>
